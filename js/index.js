dcb809e85d29aeff19c76a4830a1ce01192ead9c
Damián López <lopezrunco@gmail.com>
2022-08-10 18:45:45 +0530

    scroll-to-top  btn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import { currentYear } from "./currentYear.js"
 import { hideLoader } from "./hideLoader.js"
+import {scrollToTop} from './scrollToTop.js'
 
 const d = document
 
 d.addEventListener('DOMContentLoaded', e => {
     currentYear()
     hideLoader()
+    scrollToTop('.scroll-top-btn', '.scroll-top-btn i')
 })
