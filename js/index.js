af184b8a3ee9a133d875b040d779a5ecadd24cd0
Damián López <lopezrunco@gmail.com>
2022-08-13 12:22:41 +0530

    Contact form

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 import { currentYear } from "./currentYear.js"
 import { hideLoader } from "./hideLoader.js"
 import {scrollToTop} from './scrollToTop.js'
+import contactFormValidations from "./contactFormValidations.js"
 
 const d = document
 
 d.addEventListener('DOMContentLoaded', e => {
+    contactFormValidations()
     currentYear()
     hideLoader()
     scrollToTop('.scroll-top-btn', '.scroll-top-btn i')
